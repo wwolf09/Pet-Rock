68179934adab7beff3a05c737c2f04fbad541d32
Charlene Joy Fraginal <jgtvchannel11@gmail.com>
2024-10-04 21:46:22 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,104 @@
+from math import trunc
+
+import discord
+from Cryptodome.SelfTest.Hash.test_cSHAKE import descr
+from discord import app_commands
+from discord.ext import commands
+import random
+import dctoken
+from datetime import datetime
+import pytz
+import pickledb
+import asyncio
+
+# Initialize the Discord client
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+client = discord.Client(command_prefix=',', intents=intents)
+tree = app_commands.CommandTree(client)
+hasPet = pickledb.load('rock', True)
+stats = pickledb.load('stats', True)
+
+@client.event
+async def on_ready():
+    await tree.sync()
+    print('Bot is ready.')
+
+    if not hasPet.exists('hasPet'):
+        hasPet.lcreate('hasPet')
+        stats.dcreate('stats')
+
+async def returnstatus(id):
+    name = await client.fetch_user(id)
+    return_this = discord.Embed(description= await returnStats(id),colour=discord.Color.blue(), title=((f'{name} The Rock')))
+    return_this.set_image(url="https://media.discordapp.net/attachments/769117147653210133/1291718080212369499/rock-vector-icon-on-white-background-rock-emoji-illustration-isolated-rock-stone-vector.png?ex=67011db5&is=66ffcc35&hm=c3ca212cb8b489556d258b4ea176325a7c9efef5e6932e0e1b57f71e511e5d87&=&format=webp&quality=lossless")
+    return return_this
+
+# async def returnStats(name):
+
+async def returnStats(ID):
+    print(str(ID))
+    rock_level = stats.dget(str(ID), "level")
+    rock_hp = stats.dget(str(ID), "hp")
+    print(stats.dexists(str(ID), "XP"))
+    rock_xp = stats.dget(str(ID), "XP")
+
+    return_this_nerd_lmao = f'**Level**: {rock_level} \n**HP**: {rock_hp} \nXP:{rock_xp}'
+    return return_this_nerd_lmao
+
+@client.event
+async def on_message(message: discord.Message):
+    author_id = str(message.author.id)
+    print(author_id)
+
+    if message.author.id in hasPet.lgetall('hasPet'):
+        current_xp = stats.dget(str(author_id), "XP")
+        newxp = current_xp + 3
+        print(newxp)
+        stats.dadd(author_id, ("XP", newxp))
+        print(stats.dget(str(author_id),"XP"))
+
+@tree.command(name="rock-status", description="take a look at the status of your pet rock!")
+async def rock_status(interaction: discord.Interaction):
+    list_users = hasPet.lgetall('hasPet')
+    print(list_users)
+    if interaction.user.id in list_users:
+        await interaction.channel.send(embed= await returnstatus(interaction.user.id))
+        print(stats.dget(str(interaction.user.id), "level"))
+        print(stats.dget(str(interaction.user.id), "hp"))
+        print(stats.dget(str(interaction.user.id), "XP"))
+    else:
+        hasPet.ladd('hasPet', interaction.user.id)
+        stats.dcreate(str(interaction.user.id))
+        stats.dadd(str(interaction.user.id), ("level", 1))
+        stats.dadd(str(interaction.user.id), ("hp", 100))
+        stats.dadd(str(interaction.user.id), ("XP", 1))
+        print(stats.dget(str(interaction.user.id), "level"))
+        print(stats.dget(str(interaction.user.id), "hp"))
+        print(stats.dget(str(interaction.user.id), "XP"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.run(dctoken.dc_token)
