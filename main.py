ea43f3ae4bba6f09c80a64573902916fc66715e8
Charlene Joy Fraginal <jgtvchannel11@gmail.com>
2024-10-05 18:36:04 +0800

    amazing commit btw

--- main.py
+++ main.py
@@ -35,8 +35,6 @@ async def returnstatus(id):
     return_this.set_image(url="https://media.discordapp.net/attachments/769117147653210133/1291718080212369499/rock-vector-icon-on-white-background-rock-emoji-illustration-isolated-rock-stone-vector.png?ex=67011db5&is=66ffcc35&hm=c3ca212cb8b489556d258b4ea176325a7c9efef5e6932e0e1b57f71e511e5d87&=&format=webp&quality=lossless")
     return return_this
 
-# async def returnStats(name):
-
 async def returnStats(ID):
     print(str(ID))
     rock_level = stats.dget(str(ID), "level")
@@ -50,20 +48,30 @@ async def returnStats(ID):
 @client.event
 async def on_message(message: discord.Message):
     author_id = str(message.author.id)
+    author_name = str(message.author.name)
     print(author_id)
 
     if message.author.id in hasPet.lgetall('hasPet'):
         current_xp = stats.dget(str(author_id), "XP")
+        current_level = stats.dget(str(author_id), "level")
+        required_next_xp = 100 * (current_level/2)
+
         newxp = current_xp + 3
         print(newxp)
         stats.dadd(author_id, ("XP", newxp))
         print(stats.dget(str(author_id),"XP"))
+
+        if newxp >= required_next_xp:
+            stats.dadd(author_id, ("level", (current_level + 1)))
+            new_level = stats.dget(str(author_id), "level")
+            await message.channel.send(content=f'**{author_name} The Rock** has leveled up to lvl {new_level}!')
 
 @tree.command(name="rock-status", description="take a look at the status of your pet rock!")
 async def rock_status(interaction: discord.Interaction):
     list_users = hasPet.lgetall('hasPet')
     print(list_users)
     if interaction.user.id in list_users:
+        # Send Status
         await interaction.channel.send(embed= await returnstatus(interaction.user.id))
         print(stats.dget(str(interaction.user.id), "level"))
         print(stats.dget(str(interaction.user.id), "hp"))
